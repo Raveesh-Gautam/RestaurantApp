b61fcb002fe6bba362082a3bb0d041129f3c5c6e
Raveesh-Gautam <raveeshgautamfzd2002@gmail.com>
2025-07-09 13:21:47 +0530

    header created

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="main">
+    <div className="main-div">
+      <div className="main-div-left ">ReactMeals</div>
+      <div className="main-div-right">
+        <div className="icon"></div>
+        <div className="cart">Your Cart</div>
+        <div className="count">0</div>
+      </div>
+    </div>
+<div className="main-food"></div>
+    </div>
+  );
+};
+
+export default Header;
