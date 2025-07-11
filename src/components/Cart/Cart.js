02662cc049997ddb42aa3f6c7890166384963044
Raveesh-Gautam <raveeshgautamfzd2002@gmail.com>
2025-07-11 14:04:45 +0530

    Showing and Enabling the delete of Cart Items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,55 +1,89 @@
-import React, { useContext, useState } from 'react';
-import CartContext from '../store/auth-context';
-import Modal from '../UI/Modal';
-import styles from './Cart.module.css';
- const CartData=[{id:1,name:"Shushi",price:170}];
+import React, { useContext, useState } from "react";
+import CartContext from "../store/auth-context";
+import Modal from "../UI/Modal";
+import styles from "./Cart.module.css";
+//  const CartData=[{id:1,name:"Shushi",price:170}];
 const Cart = (props) => {
-   const cartCtx= useContext(CartContext);
-   console.log("Cart",cartCtx.items)
-const [cartOpen ,setCartOpen]=useState();
-    const handleClose=()=>{
-if(props.isOpen){
-    setCartOpen(false);
-    props.setIsOpen(false);
-}else{
-    setCartOpen(true);
-     props.setIsOpen(true);
-}
+  const cartCtx = useContext(CartContext);
+  console.log("Cart", cartCtx.items);
+  const [cartOpen, setCartOpen] = useState();
+  const handleClose = () => {
+    if (props.isOpen) {
+      setCartOpen(false);
+      props.setIsOpen(false);
+    } else {
+      setCartOpen(true);
+      props.setIsOpen(true);
     }
-    const handleOrder=()=>{
+  };
+  const handleOrder = () => {};
 
-    }
+  const handleIncrease = (item) => {
+  cartCtx.addItem({
+    ...item,
+    quantity: 1,
+  });
+};
+
+
+console.log("ravish",cartCtx.items);
+const handleDecrease = (id) => {
+  cartCtx.removeItem(id);
+};
+
   return (
     <React.Fragment>
-        {props.isOpen&&
-    <Modal>
- <ul className={styles.ul}>
-  {cartCtx.items.map((ele, index) => (
-    <>
-   <li  key={index}>
-      {ele.name} 
-    </li>
-    <hr/>
-    </>
-    
-  ))}
-  
-</ul>
+      {props.isOpen && (
+        <Modal>
+          <ul className={styles.ul}>
+            {cartCtx.items.map((ele, index) => (
+              <>
+                <div className={styles.handleCardPrice}>
+                  <div className={styles.nameAndPrice}>
+                    <li className={styles.name} key={index}>
+                      {ele.name}
+                    </li>
+                    <div className={styles.price}>${(ele.price*(ele.quantity)).toFixed(3)}</div>
+                  </div>
+                  <div className={styles.quantity_handle}>x{ele.quantity}</div>
+                  <div className={styles.handle_button}>
+                    <button
+                      className={styles.buttonCal}
+                      onClick={()=>handleDecrease(ele.id)}
+                    >
+                      -
+                    </button>
+                    <button
+                      className={styles.buttonCal}
+                      onClick={()=>handleIncrease(ele)}
+                    >
+                      +
+                    </button>
+                  </div>
+                </div>
 
-<div className={styles.content}>
-  <span>Total Amount</span>
-  <span>${cartCtx.totalAmount}</span>
-</div>
+                <hr />
+              </>
+            ))}
+          </ul>
 
-<div className={styles.action}>
-  <button onClick={handleClose}>Close</button>
-  <button onClick={handleOrder}>Order</button>
-</div>
+          <div className={styles.content}>
+            <span>Total Amount</span>
+            <span>${cartCtx.totalAmount.toFixed(3)}</span>
+          </div>
 
-    </Modal>
-    }
+          <div className={styles.action}>
+            <button className={styles.buttonAction} onClick={handleClose}>
+              Close
+            </button>
+            <button className={styles.buttonAction} onClick={handleOrder}>
+              Order
+            </button>
+          </div>
+        </Modal>
+      )}
     </React.Fragment>
-  )
-}
+  );
+};
 
 export default Cart;
